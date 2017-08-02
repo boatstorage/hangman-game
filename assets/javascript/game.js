2e5fe3a3d0e587d10dbb594cabe9aa8585effb10
marina lyulko <marinalyulko@gmail.com>
2017-08-01 23:22:21 -0700

    JS update

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// psuedocode:
+// theme: ???
+// create different parts of the game as seperate functions/html elements
+// the parts of the game are:
+// 1) image of the guess
+// 2) words underneath 
+// create serveral words and place their letters into an array
+// create an array of arrays 
+// create a function to randomly pick an array to play 
+// create a event.key that asks to guess letters
+// place the letter into a variable for:
+wrong gueses
+
+// write a for loop to compare the variable to the current array 
+// make announcement about whter the letter is in the word
+// if matches, make letter appear. 
+
+var wordArray = []
